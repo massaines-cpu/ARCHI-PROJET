2c607b983e47df3a16661fd9f1d708b7f00a3f74
inesmassa <inesurvivor@gmail.com>
2026-02-16 14:44:54 +0100

    balavoine

--- AGENCY_FRONT/front_agence.py
+++ AGENCY_FRONT/front_agence.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
-from numpy.random import default_rng as rng
-
+import datetime
 url = ''
 def get_infections():
     pass
@@ -11,8 +10,11 @@ st.title('Inscription de nouvelles infections')
 
 if "infections" not in st.session_state:
     # st.session_state.infections = get_infections("infections")
-    st.session_state.infections = 'aigreur', 'cynisme', 'sarcasme', 'hater', 'ouin ouin'
-
+    st.session_state.infections = [
+        {"name": "aigreur", "date_infection": "2024-01-01", "level": "high"},
+        {"name": "cynisme", "date_infection": "2024-01-05", "level": "medium"},
+        {"name": "sarcasme", "date_infection": "2024-01-10", "level": "low"}
+    ]
 if "dateinfection" not in st.session_state:
     # st.session_state.infections = get_infections("infections")
     # st.session_state.dateinfection =
@@ -27,32 +29,29 @@ if "dateincubation" not in st.session_state:
     # st.session_state.infections = get_infections("infections")
     # st.session_state.dateincubation =
     pass
+df = pd.DataFrame(st.session_state.infections)
+st.table(df)
 
-nom = st.session_state.infections
-date_infection = ''
-periode = ''
-contagiousness = ''
-date_incubation = ''
-lignes = key='id_infections'
-
-#liste
-mydict = {"name":nom,"date_infection":date_infection, "detection_period":periode,"contagiousness_level":contagiousness, "date_incubation":date_incubation}
-mydata = pd.DataFrame(mydict)
-st.table(mydata)
 #ajouter
-nouvelle_infection = st.text_input("Ajouter une nouvelle infection")
-if st.button("Ajouter l'infection"):
-    if nouvelle_infection not in st.session_state.infections:
-        st.session_state.auteurs.append(nouvelle_infection)
-        st.success("Infection ajoutée : " + nouvelle_infection)
-        reponse = requests.post(url, json=nouvelle_infection)
+with st.form("ajout_infection"):
+    nouvelle_infection = st.text_input("nom de l'infection")
+    niveau = st.selectbox("niveau de contagion", ["low", "medium", "high", "critical"])
+    submit_add = st.form_submit_button("ajouter l'infection")
 
-        st.rerun()
+    if submit_add and nouvelle_infection:
+        nouvelle_donnees = {
+            "name": nouvelle_infection,
+            "date_infection": str(datetime.today()), #"%Y-%m-%d"
+            "level": niveau
+        }
 
+        st.session_state.infections.append(nouvelle_donnees)
+
+#delete
 Infection = st.selectbox(
     "Selectionner une infection à supprimer",
     st.session_state.infections,
-    key="id_infections")
+    key="infection_id")
 
 if st.button("Effacer une infection"):
 
@@ -60,19 +59,9 @@ if st.button("Effacer une infection"):
     if reponse.status_code == 200:
         pass
 
-#mettre à jour
-if st.button("Mettre à jour"):
-
-    reponse = requests.put(url, json=Infection)
-    if reponse.status_code == 200:
-        pass
-        # st.success("Ami.e ajouté.e dans la BDD")
 
 
 #map
-
-
-
 data = pd.DataFrame([[1, 43.6033755861274, 1.4397677963289235, 'grippe', '10/05/2000'],
                    [2, 43.6, 1.50, 'rhume', '23/01/1970'],
                    [3, 43.6, 1.45, 'rhume', '02/01/1980']],
@@ -89,3 +78,34 @@ df["color"] = df["infection"].apply(lambda i: couleurs[i])
 st.map(df, color='color')
 
 #retry/time out
+
+
+# nouvelle_infection = st.text_input("Ajouter une nouvelle infection")
+# if st.button("Ajouter l'infection"):
+#     if nouvelle_infection not in st.session_state.infections:
+#         st.session_state.infection.append(nouvelle_infection)
+#         st.success("Infection ajoutée : " + nouvelle_infection)
+#         reponse = requests.post(url, json=nouvelle_infection)
+#
+#         st.rerun()
+
+# nom = st.session_state.infections
+# date_infection = ''
+# periode = ''
+# contagiousness = ''
+# date_incubation = ''
+# lignes = key='id_infections'
+#
+# #liste
+# mydict = {"name":nom,"date_infection":date_infection, "detection_period":periode,"contagiousness_level":contagiousness, "date_incubation":date_incubation}
+# mydata = pd.DataFrame(mydict)
+# st.table(mydata)
+
+#mettre à jour
+# if st.button("Mettre à jour"):
+    # replace infection choisi par une nouvelle
+
+    # reponse = requests.put(url, json=Infection)
+    # if reponse.status_code == 200:
+    #     pass
+        # st.success("Ami.e ajouté.e dans la BDD")
