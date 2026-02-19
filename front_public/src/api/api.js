4fe3fdf40698b38c8cb0062c3f959cb07c04cd74
inesmassa <inesurvivor@gmail.com>
2026-02-19 17:38:20 +0100

    modif api.js pour api passerelle front

--- front_public/src/api/api.js
+++ front_public/src/api/api.js
@@ -1,17 +1,45 @@
 import axios from "axios";
+//
+//const API_CASES_URL = "http://127.0.0.1:8000";
+//const API_INFECTIONS_URL = "http://127.0.0.1:8002";
+//
+//
+//export async function getCases() {
+//  const response = await axios.get(`${API_CASES_URL}/cases`);
+//  console.log("API Data:", response.data);
+//  return response.data;
+//}
+//
+//export async function getInfections() {
+//  const response = await axios.get(`${API_INFECTIONS_URL}/infection`);
+//  console.log("API Infections Data:", response.data);
+//  return response.data;
+//}
 
+const API_PASSERELLE_URL = "http://127.0.0.1:8005";
 const API_CASES_URL = "http://127.0.0.1:8000";
 const API_INFECTIONS_URL = "http://127.0.0.1:8002";
 
-
 export async function getCases() {
-  const response = await axios.get(`${API_CASES_URL}/cases`);
-  console.log("API Data:", response.data);
-  return response.data;
+  try {
+    const response = await axios.get(`${API_PASSERELLE_URL}/cases`);
+    console.log("Cases via passerelle");
+    return response.data;
+  } catch {
+    console.warn("Passerelle down, fallback direct cases");
+    const response = await axios.get(`${API_CASES_URL}/cases`);
+    return response.data;
+  }
 }
 
 export async function getInfections() {
-  const response = await axios.get(`${API_INFECTIONS_URL}/infection`);
-  console.log("API Infections Data:", response.data);
-  return response.data;
+  try {
+    const response = await axios.get(`${API_PASSERELLE_URL}/infection`);
+    console.log("Infections via passerelle");
+    return response.data;
+  } catch {
+    console.warn("Passerelle down, fallback direct infections");
+    const response = await axios.get(`${API_INFECTIONS_URL}/infection`);
+    return response.data;
+  }
 }
